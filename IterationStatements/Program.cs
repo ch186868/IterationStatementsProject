b879f68bdeddaa76d30fb9c7e52d1aa1f8084eeb
Chris Hornbuckle <ch186868@gmail.com>
2021-01-26 17:23:14 -0600

    Iterations

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -16,19 +16,24 @@ namespace IterationStatements
             //-----START HERE------------------------------------------
             //Create a variable of type int and name it num
             //initialize the variable with a value of 0
+            var num = 0;
 
 
 
             // Create a do-while loop and use the template below:
 
+
+
+
+            do
             {
                 // Increment num by 1
-
+                num++;
                 // Then add num to the collection - numbers
-                    // Hint: reference num inside of the Add method's parentheses
-                numbers.Add(/* num */);
+                // Hint: reference num inside of the Add method's parentheses
+                numbers.Add(num);
 
-            } // <---- While your variable is less than 100
+            } while (num < 100); // <---- While your variable is less than 100
 
 
 
